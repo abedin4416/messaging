cf86a3201278050cb07babafb80749257a86cb63
Md. Amir Abedin <abedin4416@gmail.com>
2026-07-27 16:51:57 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const Pusher = require("pusher");
+const { Pool } = require("pg");
 
+const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
+});
 
 app.get("/", (req, res) => {
     res.send("Express app running from root on vercel!");
