2b0528d6ac7ea0b0665bc69736d0197cad7c95ca
Md. Amir Abedin <abedin4416@gmail.com>
2026-07-27 20:57:08 +0600

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
-
+app.listen(3000);
 module.exports = app;
