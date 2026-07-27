5a67550ee1280aefe58487876d6e780069b83169
Md. Amir Abedin <abedin4416@gmail.com>
2026-07-27 16:53:59 +0600

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const Pusher = require("pusher");
-const { Pool } = require("pg");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {rejectUnauthorized: false}
-});
 
 app.get("/", (req, res) => {
     res.send("Hello, I am Tanim.");
