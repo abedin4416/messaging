9840a1439972b22564a2ec1e980b3382936976b7
Md. Amir Abedin <abedin4416@gmail.com>
2026-07-27 17:05:42 +0600

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hi Choity! 🥱");
+    res.send("<h1>Hi Choity! 🥱<h1>");
 });
 
 app.get("/users", (req, res) => {
