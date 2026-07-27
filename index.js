9c7679bd37772407766595c132da8ba90d56c48c
Md. Amir Abedin <abedin4416@gmail.com>
2026-07-27 16:55:10 +0600

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 
 const app = express();
 app.use(express.json());
-
 
 app.get("/", (req, res) => {
     res.send("Hello, I am Tanim.");
