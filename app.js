eb78a94f7c45bd415e08ab1513e43f8eecc67b55
Md. Amir Abedin <abedin4416@gmail.com>
2026-07-29 17:28:14 +0600

    first commit

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 
-app.post("/message", (req, res) => {
-    res.json({text: "Hello, from the server."});
+app.post("/", (req, res) => {
+    res.json({signtext: "Sign in to your account."});
+});
+
+app.post("/signup", (req, res) => {
+    res.json({signtext: "Create a new account."});
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 app.listen(3000);
--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-const ch = document.querySelector("#ch");
-
 async function receive(api, method = "GET", data = null) {
     try {
         const fetchOptions = {
@@ -21,3 +19,36 @@ async function receive(api, method = "GET", data = null) {
         throw error;
     }
 }
+
+const signmsg = document.querySelector("#sign-msg");
+const nameinput = document.querySelector("#name");
+const signbtn = document.querySelector("#sign");
+
+async function updateUI(path){
+    const data = await receive(path, "POST");
+    if(path=="/"){
+        signmsg.textContent = data.signtext;
+        nameinput.classList.add("hidden");
+        signbtn.textContent = "Sign up";
+    }
+    else if(path=="/signup"){
+        signmsg.textContent = data.signtext;
+        nameinput.classList.remove("hidden");
+        signbtn.textContent = "Sign in";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    updateUI(window.location.pathname);
+});
+
+signbtn.addEventListener("click", async ()=> {
+    const path = window.location.pathname;
+    const targetPath = path === "/"? "/signup":"/";
+    history.pushState({path: targetPath}, "", targetPath);
+    updateUI(targetPath);
+});
+
+window.addEventListener("popstate", async ()=> {
+    updateUI(window.location.pathname);
+})
