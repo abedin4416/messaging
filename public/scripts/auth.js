50905d68aa99ee5b7d31434c4d972a8749d5bf96
Md. Amir Abedin <abedin4416@gmail.com>
2026-08-05 17:50:25 +0600

    pusher test

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -26,6 +26,11 @@ async function updateUI(path){
             $("#profile-btn").style.background="url("+data.user.profilepic+")";
             $("#profile-btn").style.backgroundSize = "100%";
 
+            await server("/send", "POST", {
+                receiver:"trump",
+                content: "Hello, fuck"
+            });
+
             const chatPartner = prompt("username");
             const res = await fetch("/api/me", {credentials: "include"});
             const {username: currentUser } = await res.json();
@@ -43,11 +48,6 @@ async function updateUI(path){
 
             channel.bind("new-message", (data)=>{
                 alert(data.content);
-            });
-
-            await server("/send", "POST", {
-                receiver:"trump",
-                content: "Hello, fuck"
             });
         }
         else {
